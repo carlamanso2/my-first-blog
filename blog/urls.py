6da4887e9bffa0b981efa5332e739c0a6d267481
Carla <carlamansorojas@gmail.com>
2023-03-10 16:35:16 +0100

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
